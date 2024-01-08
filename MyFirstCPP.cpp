0c7aa8f1e1aba4161f098db875dc16144470d828
Yogesh Parihar <yogeshparihar2105@gmail.com>
2024-01-08 15:58:02 +0530

    Created the first file about strings

---
+++ MyFirstCPP.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cstring>
+
+using namespace std;
+
+int main()
+{
+    char *s;
+
+    cout<<"Enter a string : ";
+    cin>>s;
+
+    cout<<"Length "<<strlen(s)<<endl;
+
+    return 0;
+}
