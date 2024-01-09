bbf868280fd65ac487dae38c889253f3e8ac6782
Yogesh Parihar <yogeshparihar2105@gmail.com>
2024-01-09 10:35:37 +0530

    Added the whoAreYou program

---
+++ WhoAreYou.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cstring>
+
+using namespace std;
+
+int main()
+{
+    char n[20];
+    char p[20];
+    cout<<"What is your name : "<<endl;
+    cin>>n;
+
+    cout<<"Where do you live : "<<endl;
+    cin>>p;
+
+    cout<<"hello, "<<n<<" hope "<<p<<" is good place"<<endl;
+
+    return 0;
+}
