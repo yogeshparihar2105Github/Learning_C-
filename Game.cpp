f2b1961e9e494872e313248177869a7bb8793996
Yogesh Parihar <yogeshparihar2105@gmail.com>
2024-01-10 21:03:39 +0530

    Made a game That you can't play bcs I am busy, didn't get the time to complete

---
+++ Game.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<cstring>
+
+using namespace std;
+
+int main()
+{
+    char s[50] = "Hello, This is a Game about you Tell me about you";
+
+    cout<<s<<endl;
+
+    char t[50];
+
+    cin>>t;
+
+    cout<<"Hello, "<<t<<endl;
+
+    cout<<"We will play game later when My Creator get some time to make me able to play with you";
+
+    return 0;
+}
