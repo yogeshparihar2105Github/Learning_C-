b8fb5c26fd463232f719fdd22ec6bc20ea5cf35b
Yogesh Parihar <yogeshparihar2105@gmail.com>
2024-01-30 09:08:56 +0530

    modify the memory allocation code

--- MemoryAllocationInHeap.cpp
+++ MemoryAllocationInHeap.cpp
@@ -15,23 +15,18 @@ int main()
 {
     //This will create the Struct Rectangle in Stack Memory
     struct Rectangle r = {10,5};
-
     Rectangle *p = &r;
-
     p->breadth;
     p->length;
 
     //If you want to create in Heap Memory in c
     struct Rectangle *p1;
-   
     p1 = (struct Rectangle *)malloc(sizeof(struct Rectangle));
-
     p1->breadth = 10;
     p1->length = 5;
 
     //If you want to do the above in c++
     struct Rectangle *p2;
-   
     p2 = new Rectangle;
     p2->breadth = 10;
     p2->length = 5;
