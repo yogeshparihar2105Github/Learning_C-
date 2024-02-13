13752ccbc840ad28503904fc3a94d1122390b338
Yogesh Parihar <yogeshparihar2105@gmail.com>
2024-02-13 11:36:37 +0530

    Implemented array

---
+++ Data Structures/Arrays_Implementation.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+struct Arrays_Implementation
+{
+    int *A;
+    int size;
+    int length;
+};
+
+
+int main()
+{
+    struct Arrays_Implementation A;
+    A.size = 10;
+    A.length = 15;
+    return 0;
+}
