433377fb431a5d94fe8819be8c22eb39563b9927
Yogesh Parihar <yogeshparihar2105@gmail.com>
2024-02-19 21:38:03 +0530

    modified the Arrays_Implementation file

--- Data Structures/Arrays_Implementation.cpp
+++ Data Structures/Arrays_Implementation.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 
+using namespace std;
+
 //This is how the array is implemented
 struct Arrays_Implementation
 {
@@ -14,5 +16,7 @@ int main()
     struct Arrays_Implementation A;
     A.size = 10;
     A.length = 15;
+
+    cout<<A.size;
     return 0;
 }
